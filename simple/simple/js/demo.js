994041d1dd0b3e06197beb5e6a49bdcb563743b4
htobenothing <htobenothing@gmail.com>
2016-02-02 22:00:44 +0800

    demo for AngularJs

---
+++ simple/simple/js/demo.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.min.js" />
+
+    var myApp = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope) {
+            var employees = [
+                { firstName: "David1", lastName: "Hook", gender: "Male" },
+                { firstName: "David2", lastName: "Hook", gender: "Male"},
+                { firstName: "David3", lastName: "Hook" ,gender: "Male"}
+            ]
+            var country = {
+                name: "China",
+                Capital: "Beijing",
+                flag: "/img/cnlarge.gif"
+            }
+            $scope.employees = employees;
+            $scope.country = country;
+
+        });
+
